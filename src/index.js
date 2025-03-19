5040abc517fe1fe6ab6b599297ce9fd018d381a2
SergioVentura1 <sergio.venturaxd@gmail.com>
2025-03-18 23:30:35 -0600

    subir todo

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './style.css';
+import App from './App';  // Asegúrate de que esta importación solo aparezca una vez
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Crea el root correctamente
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Si deseas medir el rendimiento, puedes usar reportWebVitals
+reportWebVitals();
+
+document.querySelectorAll('button.square').forEach(button => {
+  if (button.textContent === 'X') {
+    button.classList.add('x');
+  } else if (button.textContent === 'O') {
+    button.classList.add('o');
+  }
+});
